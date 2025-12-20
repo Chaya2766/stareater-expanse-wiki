e45b0822e6072560605eff88938562f4a3368651
Chaya2766 <chaya@chaya.chaya>
2025-12-20 22:58:33 +0000

    gif rendering script

---
+++ rendering_zone/render_animation.sh
@@ -0,0 +1,7 @@
+if [ "$#" -ne 3 ]; then
+echo 'usage is like this: bash render_scene.sh [filename].scad [number of frames] [framerate]'
+echo 'inclide the .scad extension in the filename, if you want to render a "droid.scad", type the whole file name, not just droid'
+exit 1
+fi
+openscad $1 -o frames/frame.png --colorscheme=Starnight --imgsize=1000,1000 --animate=$2
+ffmpeg -framerate $3 -i frames/frame%05d.png -loop 0 -y animation_result.gif
