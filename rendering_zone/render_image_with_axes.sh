2b3abff9dab687b0333e3700c9a78c24f2244ebc
Chaya2766 <chaya@chaya.chaya>
2026-01-03 17:37:01 +0000

    Light drone
    - added a page about Drones in the Felicity tech tree
    - main Felicity tech tree page now links to the drone page
    - described software used for drone control
    - described the light drone, with a small table of stats
    - uploaded the document for the light drone and linked it on the drones page
    - uploaded an animated 360 view of the light droid
    
    - uploaded scripts for rendering animations (with and without axes)

---
+++ rendering_zone/render_image_with_axes.sh
@@ -0,0 +1,8 @@
+if [ "$#" -ne 1 ]; then
+echo 'usage is like this: bash render_image.sh [filename].scad'
+echo 'inclide the .scad extension in the filename, if you want to render a "droid.scad", type the whole file name, not just droid'
+exit 1
+fi
+openscad $1 --view=axes,scales -o render.png --colorscheme=Starnight --viewall --imgsize=1000,1000
+#ffmpeg -framerate $3 -i frames/frame%05d.png -loop 0 -y animation_result.gif
+ffmpeg -i render.png -y -vcodec libwebp -lossless 1 -preset picture -compression_level 6 render.webp
